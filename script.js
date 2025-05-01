a86e33c79d877e82a0921116711dba3dd2194966
Aseda2008 <asedanurdinova@gmail.com>
2025-05-01 17:59:50 +0600

    2025.05.01

--- script.js
+++ script.js
@@ -206,4 +206,15 @@
     
   //   return thisTimesheet;
   // }
+  //?2025.05.1
+  //!1 function monkeyCount(n) {
+  //   let result = [];
+  //   for (let i = 1; i <= n; i++) {
+  //     result.push(i);
+  //   }
+  //   return result;
+  // }
+  // console.log(monkeyCount(5));
+  
+  
 
